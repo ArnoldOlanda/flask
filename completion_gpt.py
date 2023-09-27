b36b1e293eff73a59899c8ec869ad27e9a7bce9a
ArnoldOlanda <olanda188@gmail.com>
2023-09-26 23:19:45 -0500

    Add chat history to completetion gpt

--- completion_gpt.py
+++ completion_gpt.py
@@ -2,43 +2,10 @@ import openai
 
 openai.api_key = "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"
 
-
-def initial_task():
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": """Estos son los codigos de algunos productos con sus respectivos id: 
-        10044->44
-        20010->59
-        20110->475
-        20011->60
-        20074->257
-        20073->248
-        30011->96
-        30012->97; 
-        Ahora lo que necesito es que me ayudes a generar json a partir del texto que te enviare en los proximos mensajes , 
-        necesito que entiendas el texto y sepas identificar datos como el nombre del cliente su direccion y el listado de productos que va a pedir, 
-        de acuerdo al listado de codigo y sus respectivos ids de los productos necesito que armes el listado de los productos a pedir adicionalmente a la cantidad de cada producto y sus respectivo precio, 
-        toda esa info estara en el texto que te proporcionare lo unico que debes cambiar es el codigo por el respectivo id que le corresponde""",
-            }
-        ],
-    )
-
-    print(completion.choices[0].message.content)
-
-
-# initial_task()
-
-
-def completion_gpt(content=""):
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "assistant",
-                "content": """
+message_history = [
+    {
+        "role": "user",
+        "content": """
                     Estos son los codigos de algunos productos con sus respectivos id: 
                         10044->44
                         20010->59
@@ -52,10 +19,25 @@ def completion_gpt(content=""):
                     necesito que entiendas el texto y sepas identificar datos como el nombre del cliente su direccion y el listado de productos que va a pedir, 
                     de acuerdo al listado de codigo y sus respectivos ids de los productos necesito que armes el listado de los productos a pedir adicionalmente a la cantidad de cada producto y sus respectivo precio, 
                     toda esa info estara en el texto que te proporcionare lo unico que debes cambiar es el codigo por el respectivo id que le corresponde, retorname solo el JSON generado""",
-                "role": "user",
-                "content": content,
-            }
-        ],
+    },
+    {
+        "role": "assistant",
+        "content": """
+        Entiendo que necesitas generar un JSON a partir de un texto que contiene información sobre el nombre del cliente, 
+        su dirección y un listado de productos con códigos que deben convertirse en sus respectivos IDs, 
+        además de la cantidad y el precio de cada producto. Para ayudarte con esto, necesitaré que proporciones el texto con la información específica,
+         y a partir de ese texto, te mostraré cómo generar el JSON.
+        Por favor, proporciona el texto con la información necesaria, y estaré encantado de ayudarte a crear el JSON correspondiente.
+        """,
+    },
+]
+
+
+def completion_gpt(content=""):
+    message_history.append({"role": "user", "content": content})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message_history,
         temperature=0,
         max_tokens=256,
     )
