4ec442b2b256911b5feaa57468ac93f3136430e5
ArnoldOlanda <olanda188@gmail.com>
2023-09-30 15:01:23 -0500

    Formating json

--- get_customer_id.py
+++ get_customer_id.py
@@ -1,4 +1,8 @@
 import json
+from database import get_database
+
+db = get_database()
+clientes_collection = db["clientes"]
 
 with open("clientes.json", "r") as archivo:
     # Carga el contenido del archivo JSON en una variable
@@ -7,9 +11,14 @@ with open("clientes.json", "r") as archivo:
 
 def get_customer_id(ruc_dni=""):
     cliente_id = None
-    for cliente in clientes:
-        if cliente["number"] == ruc_dni:
-            cliente_id = cliente["id"]
+    # for cliente in clientes:
+    #     if cliente["number"] == ruc_dni:
+    #         cliente_id = cliente["id"]
+    cliente = clientes_collection.find_one({"number": ruc_dni})
 
-    print(cliente_id)
-    return cliente_id
+    if not (cliente):
+        print("El cliente no existe en la base de datos")
+    else:
+        cliente_id = cliente["id"]
+        print(cliente_id)
+        return cliente_id
--- get_products_info.py
+++ get_products_info.py
@@ -6,20 +6,28 @@ bearer_token = "RO5uSj0mN9D1yt7MjGdLoEHutnaAcqjHpBSXyGyV52zNCaRsJW"
 headers = {"Authorization": f"Bearer {bearer_token}"}
 
 
-def get_products_info(products_id=[]):
+def get_products_info(products_data=[]):
     responses = []
-    for id in products_id:
+    for product in products_data:
         try:
-            response = requests.get(f"{url_base}/{id}", headers=headers)
+            response = requests.get(f"{url_base}/{product['id']}", headers=headers)
             if response.status_code == 200:
                 # print(f"Solicitud exitosa para {url_base}/{id}")
                 json_data = response.json()
-                responses.append(json_data["data"])
+                responses.append(
+                    {
+                        "id": product["id"],
+                        "cantidad": product["cantidad"],
+                        "tipo_precio": product["tipo_precio"],
+                        "data": json_data,
+                    }
+                )
+                print(json_data)
             else:
                 print(
-                    f"Fallo en la solicitud para {url_base}/{id} - Código de estado: {response.status_code}"
+                    f"Fallo en la solicitud para {url_base}/{product} - Código de estado: {response.status_code}"
                 )
         except Exception as e:
-            print(f"Error al realizar la solicitud para {url_base}/{id}: {str(e)}")
+            print(f"Error al realizar la solicitud para {url_base}/{product}: {str(e)}")
 
     return responses
